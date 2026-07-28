504ef24f76dbf69d5b7f00ab8ddbcb9108155e4e
JAY <232252631+jayjha-77@users.noreply.github.com>
2026-07-29 04:38:40 +0530

    final version

--- game.js
+++ game.js
@@ -1,96 +1,186 @@
-//console.log("hello jay");
+//human choice 
+const button = document.querySelector(".btn")
+
+let userChoice;
+let computerChoice;
+let runCount = 0;
+
+function getUserChoice(){
+    
+button.addEventListener("click", function(e){
+    let target = e.target;
+       
+    switch(target.id)
+    {
+        case "b1" :
+            console.log("Rock is choosen");
+            div_human.textContent = "YOU CHOOSE ROCK";
+            userChoice = "Rock";
+            getComputerChoice();
+            break;
+        case "b2" :
+            console.log("paper is choosen");
+            div_human.textContent = "YOU CHOOSE PAPER";
+            userChoice = "Paper";
+            getComputerChoice();
+            break;
+        case "b3" :
+            console.log("Scissor is choosen");
+            div_human.textContent = "YOU CHOOSE SCISSOR";
+            userChoice = "Scissor";
+            getComputerChoice();
+            break;
+    }
+});
+
+}
 
 
 //computer choice :-
+
 function getComputerChoice() {
     let choiceOfComputer = Math.random();
+    computerChoice;
     if(choiceOfComputer <=0.3) {
-        return 1 // 1 stands for Rock
+        computerChoice = "Rock";
+        div_computer.textContent = "JARVIS CHOOSE ROCK";
     }
 
     else if(choiceOfComputer > 0.3 && choiceOfComputer <= 0.6) {
-        return 2 // 2 stands for paper
+        computerChoice = "Paper";
+        div_computer.textContent = "JARVIS CHOOSE PAPER";
     }
 
     else {
-        return 3 // 3 stands for scissor
+        computerChoice = "Scissor";
+        div_computer.textContent = "JARVIS CHOOSE SCISSOR";
     }
+
+    console.log("computer choose " , computerChoice);
+    playround();
 }
 
-// Human's choice
-function getHumanChoice() {
-    let choice = Number(prompt("Enter your choice : 1 for Rock, 2 for paper and 3 for scissor"));
-   //input validation
-   while( choice !==2 && choice !== 1 && choice !== 3){
-    choice = Number(prompt("Enter your choice : 1 for Rock, 2 for paper and 3 for scissor"))
-    
-   }
-    return choice;
-}
-
-//player's score 
+//player's score
 let humanScore = 0;
 let computerScore = 0;
 
 
-//logic for one round
+//game logic
+function playround(){
+ runCount++;
+ round.textContent = "ROUND : " + runCount;
 
-function playround( humanChoice = getHumanChoice() , computerChoice = getComputerChoice()) {
-    //choice display
-    console.log("your choice = ",humanChoice);
-    console.log("computer choice = ", computerChoice);
-    
-    
-    if( humanChoice ==computerChoice){
+
+    let choice_human = userChoice;
+    let choice_computer = computerChoice;
+
+    if(choice_human ==choice_computer){
         console.log("It is Tie");
-        return ;
+        div_result.textContent = "IT IS TIE👾";
     }
    
-    else if(humanChoice ==1 ){
-        if(computerChoice == 2){
-            console.log("Oh, you lose");
+    else if(choice_human =="Rock" ){
+        if(choice_computer == "Paper"){
+            console.log("OH, YOU LOSE💔");
+            div_result.textContent = "OH, YOU LOSE💔";
             computerScore++ ;
         }
         else{
-            console.log("yah, you win");
+            console.log("YAH< YOU WIN😎");
+            div_result.textContent = "YAH< YOU WIN😎";
             humanScore++ ;
         }
     
     }
 
-    else if( humanChoice ==2){
-        if(computerChoice == 3){
-            console.log("Oh, you lose");
+    else if(choice_human =="Paper"){
+        if(choice_computer == "Scissor"){
+            console.log("OH, YOU LOSE💔");
+            div_result.textContent = "OH, YOU LOSE💔";
             computerScore++ ;
         }
 
         else{
-            console.log("yah, you win");
+            console.log("YAH< YOU WIN😎");
+            div_result.textContent = "YAH YOU WIN😎";
             humanScore++ ;
         }
     }
 
-    else if( humanChoice == 3){
-        if( computerChoice ==1){
-            console.log("Oh, you lose");
+    else if(choice_human == "Scissor"){
+        if( choice_computer =="Rock"){
+            console.log("OH, YOU LOSE💔");
+            div_result.textContent = "OH, YOU LOSE";
             computerScore++ ;
         }
 
         else{
-            console.log("yah, you win");
+            console.log("YAH< YOU WIN😎");
+            div_result.textContent = "YAH< YOU WIN😎";
             humanScore++ ;
         }
     }
-}
+ score.textContent = "You = " + humanScore + "  V/S   Computer = " + computerScore ;
 
-function playgame(){
-    let i = 0;
-    while(i<5){
-        playround();
-        i++;
+    if( runCount ==5){
+        winner();
+    }
+    else{
+        div_finResult.textContent = "";
     }
 }
 
-playgame();
-console.log( "you = ", humanScore ," V/s ", "computer = ", computerScore);
+//winner
+function winner(){
+    console.log("winner declaration")
+    if(humanScore > computerScore)
+    {
+        console.log(" You Rock");
+        div_finResult.textContent = " WINNER, YOU ROCK🙌";
+    }
 
+    else if( humanScore < computerScore)
+    {
+        console.log("Oh, Computer win");
+        div_finResult.textContent = "TRY NEXT TIME😢";
+    }
+
+    else{
+        console.log("It is Tie");
+        div_finResult.textContent = "sweet TIE😵‍💫";
+    }
+    
+    runCount = 0;
+    humanScore = 0;
+    computerScore =0 ;
+    
+}
+
+getUserChoice();
+
+
+//Dom manipulation for result
+ const div_human = document.querySelector("#human");
+ const div_computer = document.querySelector("#computer");
+ const div_result = document.querySelector("#result");
+ const div_finResult = document.querySelector("#finalResult");
+ const button12 = document.querySelector("#top");
+ const score = document.querySelector("#score");
+ const btn = document.querySelector(".btn");
+
+ button12.addEventListener("mouseover", () =>{
+    button12.textContent ="NOT ME 😒";
+    button12.addEventListener("mouseout", () =>{
+        button12.textContent = "TAP TO PLAY";
+    })
+ }
+)
+
+score.addEventListener("mouseover", () =>{
+    score.textContent ="AGAIN, NOT ME🤦‍♂️";
+    score.addEventListener("mouseout",() =>{
+        score.textContent = "SCORE-BOARD";
+    } )
+})
+
+const round = document.querySelector("#round")
